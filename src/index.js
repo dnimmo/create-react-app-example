e7d549a2ef8bb244e7c92c5eb4154fdda1ce5e4a
David Nimmo <dnimmo@gmail.com>
2017-08-20 15:55:35 +0100

    updated to use react-router-redux, fails because of `history` being undefined

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import { Provider } from 'react-redux';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import createHistory from 'history/createBrowserHistory';
 import registerServiceWorker from './registerServiceWorker';
 import { reducer } from './state/store'
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const store = createStore(combineReducers({ ...reducer, router: routerReducer}), applyMiddleware(middleware));
 
 ReactDOM.render(
   <Provider store={store} >
-    <BrowserRouter>
+    <ConnectedRouter history={history}>
       <App />
-    </BrowserRouter>
+    </ConnectedRouter>
   </Provider>, 
   document.getElementById('root'));
 
