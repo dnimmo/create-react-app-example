0b8b960a938d259d3559a963ea3e5bbbdb7f9938
David Nimmo <dnimmo@gmail.com>
2017-08-28 22:56:16 +0100

    assets being cached correctly using `npm start`

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,25 +1,30 @@
+const version = 'v1';
+
 self.addEventListener('install', (event) => {
-  console.log(`SW installed at ${new Date().toLocaleTimeString()}`);
+  event.waitUntil(
+    caches.open(version)
+      .then(cache => cache.addAll([
+        './index.html',
+        './global.css',
+        './static/js/bundle.js',
+      ]),
+      ),
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log(`SW activated at ${new Date().toLocaleTimeString()}`);
-});
+// self.addEventListener('activate', (event) => {
+//   console.log(`SW activated at ${new Date().toLocaleTimeString()}`);
+// });
 
 self.addEventListener('fetch',
   (event) => {
-    if (!navigator.onLine) {
-      event.respondWith(
-        new Response('<h1> You are offline! :( </h1>',
-          {
-            headers: {
-              'Content-Type': 'text/html',
-            },
-          },
-        ),
-      );
-    } else {
-      event.respondWith(fetch(event.request));
-    }
+    event.respondWith(
+      caches.match(event.request)
+        .then((res) => {
+          if (res) return res;
+          if (!navigator.onLine) return caches.match(new Request('/index.html'));
+          return fetch(event.request);
+        }),
+    );
   },
 );
