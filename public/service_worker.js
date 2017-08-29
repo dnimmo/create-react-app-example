40ad21cd9da11c5ec41ef1c6f619df1522790c46
David Nimmo <dnimmo@gmail.com>
2017-08-29 21:48:22 +0100

    caching all assets - still only from npm start for js though (only static/bundle.js)

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,5 +1,16 @@
 const version = 'v1';
 
+const fetchAndUpdateCache = request => fetch(request)
+  .then((response) => {
+    if (response) {
+      return caches.open(version)
+        .then(cache => cache.put(request, response.clone())
+          .then(() => response),
+        );
+    }
+    return console.error('No response');
+  });
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(version)
@@ -12,18 +23,29 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// self.addEventListener('activate', (event) => {
-//   console.log(`SW activated at ${new Date().toLocaleTimeString()}`);
-// });
+self.addEventListener('activate', (event) => {
+  // Delete any cached assets that don't match the current version
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(x => x !== version)
+          .map(y => caches.delete(y)),
+      ),
+      ),
+  );
+});
 
 self.addEventListener('fetch',
   (event) => {
+    // on 'fetch', check cache for matching asset, 
+    // else if offline return default cached index, 
+    // else pass through to fetchAndUpdate
     event.respondWith(
       caches.match(event.request)
         .then((res) => {
           if (res) return res;
-          if (!navigator.onLine) return caches.match(new Request('/index.html'));
-          return fetch(event.request);
+          if (!navigator.onLine) return caches.match(event.request);
+          return fetchAndUpdateCache(event.request);
         }),
     );
   },
